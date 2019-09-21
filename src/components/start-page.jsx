f659a1d54f959f41e49c9c2709866e058584cef4
Camille Hodoul <camille.hodoul@gmail.com>
2019-09-21 17:20:11 +0200

    Version 0.2.2

--- src/components/start-page.jsx
+++ src/components/start-page.jsx
@@ -82,6 +82,7 @@ function StartPage(props: { hasXmlFiles: boolean, classes: any }) {
                     <ExpansionPanelDetails>
                         <Typography className={classes.changelog}>
                             <ul>
+                                <li>{"Nouveaux traitements (origination>name, span)"}</li>
                                 <li>{"Amélioration des performances de l'export controlaccess"}</li>
                                 <li>{"Corrections controlaccess par csv : il est possible d'ajouter des attributs"}</li>
                                 <li>{"Nouvelle catégorie de traitements : 'Personnaliser'"}</li>
