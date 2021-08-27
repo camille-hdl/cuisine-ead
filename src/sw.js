5edd7818342d07ff08aba8ad193b79d34b021dbd
Camille Hodoul <camille.hodoul@gmail.com>
2021-08-27 10:50:15 +0200

    v1.1.4

--- src/sw.js
+++ src/sw.js
@@ -6,7 +6,7 @@ const { CacheableResponsePlugin } = workbox.cacheableResponse;
 const { ExpirationPlugin } = workbox.expiration;
 const { skipWaiting, clientsClaim, setCacheNameDetails } = workbox.core;
 
-var VERSION = "v1.1.3";
+var VERSION = "v1.1.4";
 setCacheNameDetails({
     prefix: "cuisine-ead",
     suffix: VERSION,
